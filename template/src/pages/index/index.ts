6ab142260c8ce763b789dcc38eea71a84399dde8
zacksleo <zacksleo@gmail.com>
2018-06-07 17:11:51 +0800

    refact(template): update template

--- template/src/pages/index/index.ts
+++ template/src/pages/index/index.ts
@@ -1,7 +1,7 @@
-import { Vue, Component } from 'vue-property-decorator'
-import { wxp as wx } from '@minapp/wx/wxp'
-import Card from '@/components/card.vue' // mpvue目前只支持的单文件组件
-import CompB from '@/components/compb.vue' // mpvue目前只支持的单文件组件
+import { Component,Vue } from 'vue-property-decorator';
+import { wxp as wx } from '@minapp/wx/wxp';
+import Card from '@/components/card.vue'; // mpvue目前只支持的单文件组件
+import CompB from '@/components/compb.vue'; // mpvue目前只支持的单文件组件
 
 // 必须使用装饰器的方式来指定component
 @Component({
